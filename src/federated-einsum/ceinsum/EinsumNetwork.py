fix gradient bug

src/federated-einsum/ceinsum/EinsumNetwork.py
@@ -72,7 +72,7 @@ class EinsumNetwork(torch.nn.Module):
         super(EinsumNetwork, self).__init__()
 
         self.param_nn = param_nn
-
+        self.last_params = None
         check_flag, check_msg = Graph.check_graph(graph)
         if not check_flag:
             raise AssertionError(check_msg)
@@ -157,6 +157,7 @@ class EinsumNetwork(torch.nn.Module):
         y_oh = F.one_hot(y, num_classes=1000)
         x_prev = torch.cat([x_prev, y_oh], dim=1)
         params = self.param_nn(x_prev)
+        self.last_params = params
         x_in = x_in.permute((0, 2, 3, 1))
         x_in = x_in.reshape(x_in.shape[0], config.num_vars, config.num_dims)
 

src/federated-einsum/ceinsum/ExponentialFamilyArray.py
@@ -68,7 +68,7 @@ class ExponentialFamilyArray(torch.nn.Module):
         self.marginalization_idx = None
         self.marginalization_mask = None
 
-        self._use_em = use_em
+        self._use_em = False #use_em
         self._p_acc = None
         self._stats_acc = None
         self._online_em_frequency = None
@@ -230,7 +230,6 @@ class ExponentialFamilyArray(torch.nn.Module):
         :return: log-densities of implemented exponential family (Tensor).
                  Will be of shape (batch_size, self.num_var, *self.array_shape)
         """
-
         if self._use_em:
             #with torch.no_grad():
             theta = self.expectation_to_natural(params)